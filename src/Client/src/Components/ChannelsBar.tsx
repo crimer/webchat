7bb027ad3e47dd3f89689501f60096e4cbd6250f
crimer <99nik.pro@gmail.com>
2020-11-23 15:09:00 +1000

    Создал модальное окно создания чата

--- src/Client/src/Components/ChannelsBar.tsx
+++ src/Client/src/Components/ChannelsBar.tsx
@@ -1,16 +1,25 @@
 import {
     Divider,
+    IconButton,
     List,
     ListItem,
     ListItemIcon,
     ListItemText,
     makeStyles,
+    Menu,
+    MenuItem,
     Theme,
     Typography,
-    useTheme,
 } from '@material-ui/core'
+import CreateIcon from '@material-ui/icons/Create'
 import Drawer from '@material-ui/core/Drawer/Drawer'
 import React, { useState } from 'react'
+import GroupIcon from '@material-ui/icons/Group'
+import ChatIcon from '@material-ui/icons/Chat'
+import RadioIcon from '@material-ui/icons/Radio'
+import CreateChatModal, {ChatTypes,CreateChatType} from '../Components/CreateChatModal'
+
+
 
 const drawerWidth = 320
 
@@ -31,41 +40,121 @@ const useStyles = makeStyles((theme: Theme) => ({
         padding: theme.spacing(0, 1),
         ...theme.mixins.toolbar,
     },
+    options: {
+        padding: theme.spacing(1),
+    },
+    createChatIcon: {
+        color: '#00e676',
+    },
+    chatIcon: {
+        marginRight: '10px',
+    },
+    avatarSize: {
+        width: theme.spacing(15),
+        height: theme.spacing(15),
+        fontSize: '45px',
+    },
+    gridItem: {
+        minWidth: '500px',
+    },
+    paper: {
+        padding: theme.spacing(3, 2),
+    },
 }))
 
 type ChannelsBarProps = {}
 
 const ChannelsBar: React.FC<ChannelsBarProps> = () => {
     const classes = useStyles()
-    const channels = [
-        'DotNetChatRu',
-        'VueJs',
-        'React RU',
-        'Xamarin Developers',
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const [isCreateChatOpen, setCreateChatOpen] = useState<boolean>(false)
+    const [selectedChatType, setSelectedChatType] = useState<CreateChatType | undefined>()
+
+    const channels = ['DotNetChatRu', 'VueJs', 'React RU', 'Xamarin Developers']
+
+    const channelTypes: CreateChatType[] = [
+        {
+            id: 1,
+            type: ChatTypes.Group,
+            text: 'Группа',
+            icon: <GroupIcon className={classes.chatIcon} />,
+        },
+        {
+            id: 2,
+            type: ChatTypes.Channel,
+            text: 'Канал',
+            icon: <RadioIcon className={classes.chatIcon} />,
+        },
+        {
+            id: 3,
+            type: ChatTypes.Direct,
+            text: 'Личная переписка',
+            icon: <ChatIcon className={classes.chatIcon} />,
+        },
     ]
+    const openCreateChatMenu = (event: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget)
+    const handleClose = () => setAnchorEl(null)
+
+    const selectChatType = (chatType: CreateChatType) => {
+        setAnchorEl(null)
+        setCreateChatOpen(true)
+        setSelectedChatType(chatType)
+    }
+    const modalClose = () => {
+        setCreateChatOpen(false)
+        setSelectedChatType(undefined)
+    }
+
     return (
-        <Drawer
-            variant='permanent'
-            className={classes.drawer}
-            classes={{
-                paper: classes.drawerPaper,
-            }}
-            anchor='left'>
-            <div className={classes.drawerHeader}>
-                <Typography>Чаты:</Typography>
-            </div>
-            <Divider />
-            <List>
-                {channels.map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>
-                            {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-        </Drawer>
+        <>
+            <CreateChatModal open={isCreateChatOpen} createChatType={selectedChatType} onModalClose={modalClose}/>
+            <Drawer
+                variant='permanent'
+                className={classes.drawer}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+                anchor='left'>
+                <div className={classes.options}>
+                    <IconButton
+                        aria-label='createChat'
+                        onClick={openCreateChatMenu}>
+                        <CreateIcon
+                            fontSize='default'
+                            className={classes.createChatIcon}
+                        />
+                    </IconButton>
+                    <Menu
+                        id='create-chats-menu'
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}>
+                        {channelTypes.map((type) => (
+                            <MenuItem
+                                onClick={() => selectChatType(type)}
+                                key={type.type}>
+                                {type.icon}
+                                {type.text}
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                </div>
+                <div className={classes.drawerHeader}>
+                    <Typography>Чаты:</Typography>
+                </div>
+                <Divider />
+                <List>
+                    {channels.map((text) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+        </>
     )
 }
 export default ChannelsBar
---
+++ src/Client/src/Components/CreateChatModal.tsx
@@ -0,0 +1,244 @@
+import {
+    Avatar,
+    Button,
+    createStyles,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    FormControl,
+    Grid,
+    Input,
+    InputLabel,
+    makeStyles,
+    MenuItem,
+    Paper,
+    Select,
+    TextField,
+    Theme,
+    useTheme,
+} from '@material-ui/core'
+import React, { useMemo, useState } from 'react'
+import Chip from '@material-ui/core/Chip'
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        formControl: {
+            margin: theme.spacing(1),
+            minWidth: 120,
+            maxWidth: 300,
+        },
+        chips: {
+            display: 'flex',
+            flexWrap: 'wrap',
+        },
+        chip: {
+            margin: 2,
+        },
+        noLabel: {
+            marginTop: theme.spacing(3),
+        },
+        avatarSize: {
+            width: theme.spacing(15),
+            height: theme.spacing(15),
+            fontSize: '45px',
+        },
+        gridItem: {
+            minWidth: '500px',
+        },
+        paper: {
+            padding: theme.spacing(3, 2),
+        },
+        chatType: {
+            marginTop: '20px',
+            display: 'flex',
+            alignItems: 'center',
+            '& p': {
+                marginRight: '10px',
+            },
+        },
+    })
+)
+
+function getStyles(name: string, personName: string[], theme: Theme) {
+    return {
+        fontWeight:
+            personName.indexOf(name) === -1
+                ? theme.typography.fontWeightRegular
+                : theme.typography.fontWeightMedium,
+    }
+}
+
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+}
+
+export enum ChatTypes {
+    Direct,
+    Group,
+    Channel,
+}
+export type CreateChatType = {
+    id: number
+    type: ChatTypes
+    text: string
+    icon: any
+}
+
+type CreateChatModalType = {
+    open: boolean
+    createChatType: CreateChatType | undefined
+    onModalClose: () => void
+}
+
+const CreateChatModal: React.FC<CreateChatModalType> = ({
+    open,
+    createChatType,
+    onModalClose,
+}) => {
+    const theme = useTheme()
+    const classes = useStyles()
+
+    const [chatSettings, setChatSetting] = useState<{
+        chatTitle: string
+        chatType: string
+        users: string[]
+        creator: string
+    }>({
+        chatTitle: '',
+        chatType: '',
+        users: [],
+        creator: '',
+    })
+    const names = [
+        'Oliver Hansen',
+        'Van Henry',
+        'April Tucker',
+        'Ralph Hubbard',
+        'Omar Alexander',
+        'Carlos Abbott',
+        'Miriam Wagner',
+        'Bradley Wilkerson',
+        'Virginia Andrews',
+        'Kelly Snyder',
+    ]
+
+    const chatNames = chatSettings.chatTitle.split('')
+    const chatShortName = useMemo(() => {
+        const name = chatNames.reduce(
+            (result, currentName) => (result += currentName[0].toUpperCase()),
+            ''
+        )
+        console.log('s')
+
+        return name.trim().slice(0, 1)
+    }, [chatSettings.chatTitle])
+    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        const users = event.target.value as string[]
+        setChatSetting({ ...chatSettings, users })
+    }
+    const closeModal = () => {
+        setChatSetting({
+            chatTitle: '',
+            chatType: '',
+            users: [],
+            creator: '',
+        })
+        onModalClose()
+    }
+
+    return (
+        <Dialog open={open} onClose={closeModal}>
+            <DialogTitle>Создаем новый чат</DialogTitle>
+            <DialogContent>
+                <Grid container spacing={2} justify='center'>
+                    <Grid item className={classes.gridItem}>
+                        <Paper className={classes.paper}>
+                            <Avatar
+                                alt={chatSettings.chatTitle}
+                                className={classes.avatarSize}>
+                                {chatShortName}
+                            </Avatar>
+                            <div className={classes.chatType}>
+                                <p>Тип:</p>
+                                {createChatType && createChatType.icon}
+                                {createChatType && createChatType.text}
+                            </div>
+                        </Paper>
+                    </Grid>
+                    <Grid item className={classes.gridItem}>
+                        <form>
+                            <TextField
+                                variant='outlined'
+                                margin='normal'
+                                required
+                                fullWidth
+                                value={chatSettings.chatTitle}
+                                onChange={(e) =>
+                                    setChatSetting({
+                                        ...chatSettings,
+                                        chatTitle: e.target.value,
+                                    })
+                                }
+                                label='Название чата'
+                                name='chatTitle'
+                                autoFocus
+                            />
+                            <FormControl fullWidth>
+                                <InputLabel>Участники</InputLabel>
+                                <Select
+                                    multiple
+                                    value={chatSettings.users}
+                                    onChange={handleChange}
+                                    input={<Input />}
+                                    renderValue={(selected) => (
+                                        <div className={classes.chips}>
+                                            {(selected as string[]).map(
+                                                (value) => (
+                                                    <Chip
+                                                        key={value}
+                                                        label={value}
+                                                        className={classes.chip}
+                                                    />
+                                                )
+                                            )}
+                                        </div>
+                                    )}
+                                    MenuProps={MenuProps}>
+                                    {names.map((name) => (
+                                        <MenuItem
+                                            key={name}
+                                            value={name}
+                                            style={getStyles(
+                                                name,
+                                                chatSettings.users,
+                                                theme
+                                            )}>
+                                            {name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </form>
+                    </Grid>
+                </Grid>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={closeModal} color='primary' autoFocus>
+                    Создать
+                </Button>
+                <Button onClick={closeModal} color='primary'>
+                    Отмена
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+export default CreateChatModal
